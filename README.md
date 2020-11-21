ed63fc982321792f1154c4e55b386f7193494ba4
kirtipai9 <56118423+kirtipai9@users.noreply.github.com>
2020-11-21 18:02:18 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Basic Banking System  
+A Web Application used to transfer money between multiple users.
+ 
+Flow of the Website: Home Page > View all Users > Select and View one User > Transfer Money > Select reciever > View all Users > View Transfer History.
---
+++ navigation.php
@@ -0,0 +1,35 @@
+<!-- navbar --> 
+<head>
+	<style>
+	 .main{
+		 background:black;
+	 }
+	 .btn{
+		 font-family:AERIAL;
+		 font-size:15px;
+		 color:#495057;
+	 }
+	</style>
+</head>
+<body>
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class=" btn " href="index.php">NEW BANK</a>
+     
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="home.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="newuser.php">New User</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="moneytransfer.php">Money Transfer</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php">Transaction History</a>
+              </li>
+          </div>
+       </nav>
+</div>
+</body>
